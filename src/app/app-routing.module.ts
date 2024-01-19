5a1731c737ad315a9825ca8c1f43f1481ef2d706
manuelmelchiorri96 <manuelmelchiorri1996@gmail.com>
2024-01-19 08:04:08 +0100

    configurate rotte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './components/account/account.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrazioneComponent } from './components/registrazione/registrazione.component';
+import { UtentiComponent } from './components/utenti/utenti.component';
+import { authorizationGuard } from './services/auth/authorization.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registrazione', component: RegistrazioneComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [authorizationGuard],
+  },
+  {
+    path: 'account',
+    component: AccountComponent,
+    canActivate: [authorizationGuard],
+  },
+  {
+    path: 'utenti',
+    component: UtentiComponent,
+    canActivate: [authorizationGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
