bfd6867236f33fc642023a754f00ed4d2b9f2196
manuelmelchiorri96 <manuelmelchiorri1996@gmail.com>
2024-01-19 10:38:14 +0100

    implementata registrazione component

--- src/app/components/registrazione/registrazione.component.spec.ts
+++ src/app/components/registrazione/registrazione.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegistrazioneComponent } from './registrazione.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('RegistrazioneComponent', () => {
   let component: RegistrazioneComponent;
@@ -8,10 +14,18 @@ describe('RegistrazioneComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [RegistrazioneComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+      ],
+      declarations: [RegistrazioneComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RegistrazioneComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/components/registrazione/registrazione.component.ts
+++ src/app/components/registrazione/registrazione.component.ts
@@ -1,10 +1,111 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { UserSignUp } from '../../models/interface/userSignUp';
+import { LocalStorageService } from '../../services/core/local-storage.service';
+import { HttpService } from '../../services/http-service/http.service';
 
 @Component({
   selector: 'app-registrazione',
   templateUrl: './registrazione.component.html',
-  styleUrl: './registrazione.component.css'
+  styleUrl: './registrazione.component.css',
 })
-export class RegistrazioneComponent {
+export class RegistrazioneComponent implements OnInit {
+  user: UserSignUp = {
+    name: '',
+    email: '',
+    gender: '',
+    status: '',
+  };
+  messageErr: string = '';
+  messageSuccess: string = '';
+  isLogged: boolean = false;
+  generi: any[] = ['male', 'female'];
 
+  constructor(
+    private httpService: HttpService,
+    private localStorageService: LocalStorageService
+  ) {}
+
+  ngOnInit(): void {
+    if (this.userIsLogged()) {
+      this.isLogged = true;
+    }
+  }
+
+  userIsLogged(): boolean {
+    return this.localStorageService.getIsLogged() === 'true';
+  }
+
+  nomeFormControl = new FormControl('', [Validators.required]);
+  genereFormControl = new FormControl('', [Validators.required]);
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  tokenFormControl = new FormControl('', [Validators.required]);
+
+  capitalizeFirstLetter() {
+    const nome = this.nomeFormControl.value;
+    if (nome) {
+      this.nomeFormControl.setValue(
+        nome.charAt(0).toUpperCase() + nome.slice(1)
+      );
+    }
+  }
+
+  convertEmailToLowerCase() {
+    const email = this.emailFormControl.value;
+    if (email) {
+      this.emailFormControl.setValue(email.toLowerCase());
+    }
+  }
+
+  onSubmit() {
+    const nome = this.nomeFormControl.value;
+    const genere = this.genereFormControl.value;
+    const email = this.emailFormControl.value;
+    const token = this.tokenFormControl.value;
+    const status = 'active';
+
+    if (nome && genere && email && token) {
+      this.user = {
+        name: nome,
+        email: email,
+        gender: genere,
+        status: status,
+      };
+
+      if (!this.isLogged) {
+        this.localStorageService.setToken(token);
+      }
+
+      this.httpService.registraUser(this.user).subscribe({
+        next: (data) => {
+          if (!this.isLogged) {
+            this.messageSuccess = nome + ', ti sei registrato correttamente.';
+            this.localStorageService.setName(nome);
+            this.localStorageService.setEmail(email);
+            this.localStorageService.setGenere(genere);
+            this.localStorageService.setStatus(status);
+            this.localStorageService.setId(data['id']);
+          } else {
+            this.messageSuccess =
+              this.localStorageService.getName() +
+              ', hai sei registrato correttamente ' +
+              nome;
+          }
+          this.messageErr = '';
+        },
+        error: (err) => {
+          console.error('Errore durante la richiesta:', err);
+          this.messageSuccess = '';
+          if (err.status === 401) {
+            this.messageErr = nome + ', hai messo un token non valido.';
+          } else if (err.status === 422) {
+            this.messageErr = nome + ", l'e-mail inserita è già esistente.";
+          }
+        },
+      });
+    }
+  }
 }
