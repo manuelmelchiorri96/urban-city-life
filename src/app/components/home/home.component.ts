2c61eecd3a30147c0fd1aba10e58b766753d1e12
manuelmelchiorri96 <manuelmelchiorri1996@gmail.com>
2024-01-19 11:10:08 +0100

    implementata home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocalStorageService } from '../../services/core/local-storage.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  currentIndex = 0;
+  intervalId: any;
+  slides = [
+    'assets/immagine_1.png',
+    'assets/immagine_2.jpg',
+    'assets/immagine_3.jpg',
+  ];
 
+  constructor(
+    private localStorageService: LocalStorageService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.startAutoSlide();
+
+    if (this.localStorageService.getIsLogged() !== 'true') {
+      this.router.navigate(['login']);
+    }
+  }
+
+  prevSlide() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+  }
+
+  startAutoSlide() {
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, 6000);
+  }
+
+  onArrowClick() {
+    clearInterval(this.intervalId);
+    this.startAutoSlide();
+  }
 }
