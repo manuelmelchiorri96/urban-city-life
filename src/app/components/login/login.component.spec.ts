8b58098590b1ae613c4de418e8690533873f5e57
manuelmelchiorri96 <manuelmelchiorri1996@gmail.com>
2024-01-19 10:30:02 +0100

    implementato login component

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,10 +13,17 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LoginComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatInputModule,
+      ],
+      declarations: [LoginComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LocalStorageService } from '../../services/core/local-storage.service';
+import { HttpService } from '../../services/http-service/http.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  isLogged: boolean = false;
+  error: string = '';
+  prendiNuovoToken: boolean = false;
 
+  constructor(
+    private localStorageService: LocalStorageService,
+    private router: Router,
+    private httpService: HttpService
+  ) {}
+
+  ngOnInit(): void {}
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  tokenFormControl = new FormControl('', [Validators.required]);
+  nuovoTokenFormControl = new FormControl('', [Validators.required]);
+
+  convertEmailToLowerCase() {
+    const email = this.emailFormControl.value;
+    if (email) {
+      this.emailFormControl.setValue(email.toLowerCase());
+    }
+  }
+
+  onSubmit() {
+    const email = this.emailFormControl.value;
+    const token = this.tokenFormControl.value;
+
+    if (email && token) {
+      if (
+        email === this.localStorageService.getEmail() &&
+        token === this.localStorageService.getToken() &&
+        token !== ''
+      ) {
+        this.prendiNuovoToken = true;
+        this.error = '';
+      } else if (email !== this.localStorageService.getEmail()) {
+        this.error = "L' e-mail inserita è errata";
+      } else if (token !== this.localStorageService.getToken()) {
+        this.error = 'Il token inserito è errato';
+      }
+      if (this.prendiNuovoToken && this.nuovoTokenFormControl.value) {
+        const nuovoToken = this.nuovoTokenFormControl.value;
+        this.localStorageService.setToken(nuovoToken);
+        this.error = '';
+
+        if (email === this.localStorageService.getEmail()) {
+          this.httpService.trovaUsers().subscribe({
+            next: (data) => {
+              this.isLogged = true;
+              this.localStorageService.setIsLogged(this.isLogged);
+              this.router.navigate(['home']);
+            },
+            error: (error) => {
+              console.error('Error fetching data:', error);
+              this.error = 'Nuovo token non valido';
+            },
+          });
+        }
+      }
+    }
+  }
 }
