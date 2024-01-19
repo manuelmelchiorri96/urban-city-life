29637620a71ace39a67a9f60758765506f881368
manuelmelchiorri96 <manuelmelchiorri1996@gmail.com>
2024-01-19 08:06:14 +0100

    aggiunte varie component e dichiarate nell' app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrazioneComponent } from './components/registrazione/registrazione.component';
+import { AccountComponent } from './components/account/account.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { UtentiComponent } from './components/utenti/utenti.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistrazioneComponent,
+    AccountComponent,
+    DashboardComponent,
+    LayoutComponent,
+    UtentiComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/utenti/utenti.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-utenti',
+  templateUrl: './utenti.component.html',
+  styleUrl: './utenti.component.css'
+})
+export class UtentiComponent {
+
+}
