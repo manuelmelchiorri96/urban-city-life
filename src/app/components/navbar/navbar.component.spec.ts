4ab7f2280dcb9dee5575aa74663ae65d763e986b
manuelmelchiorri96 <manuelmelchiorri1996@gmail.com>
2024-01-19 10:30:52 +0100

    implementata navbar component

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -8,10 +10,10 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [NavbarComponent]
-    })
-    .compileComponents();
-    
+      imports: [MatToolbarModule, MatIconModule],
+      declarations: [NavbarComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocalStorageService } from '../../services/core/local-storage.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  @Input() currentRouter: string = '';
+  showLogoutConfirmation: boolean = false;
+  mobileMenu: boolean = false;
 
+  constructor(
+    private router: Router,
+    private localStorageService: LocalStorageService
+  ) {}
+
+  ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {}
+
+  viewMenuMobile() {
+    if (this.mobileMenu) {
+      this.mobileMenu = false;
+    } else {
+      this.mobileMenu = true;
+    }
+  }
+
+  isCurrentPage(route: string) {
+    return this.currentRouter === route;
+  }
+
+  userIsLogged(): boolean {
+    return this.localStorageService.getIsLogged() === 'true';
+  }
+
+  startLogoutConfirmation() {
+    this.showLogoutConfirmation = true;
+  }
+
+  cancelLogout() {
+    this.showLogoutConfirmation = false;
+  }
+
+  confirmLogout() {
+    this.showLogoutConfirmation = false;
+    this.localStorageService.setIsLogged(false);
+    this.router.navigate(['login']);
+  }
 }
