7e7a432329833eca761487fd4b285a1a3189bcb3
manuelmelchiorri96 <manuelmelchiorri1996@gmail.com>
2024-01-20 12:46:02 +0100

    implemento test, attuale coverage circa 58%

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-import { Router } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { LayoutComponent } from './components/layout/layout.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavigationEnd, Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Subject } from 'rxjs';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
@@ -33,5 +34,25 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     expect(app).toBeTruthy();
+  });
+
+  it('should update currentRouter on navigation end', () => {
+    const navigationEndEvent = new NavigationEnd(1, '/', '/dummy');
+
+    app.prendiURL();
+
+    (router.events as Subject<any>).next(navigationEndEvent);
+
+    expect(app.currentRouter).toBe('');
+  });
+
+  it('should not update currentRouter on non-navigation events', () => {
+    const nonNavigationEvent = new Event('some-event');
+
+    app.prendiURL();
+
+    (router.events as Subject<any>).next(nonNavigationEvent);
+
+    expect(app.currentRouter).toBe('');
   });
 });
